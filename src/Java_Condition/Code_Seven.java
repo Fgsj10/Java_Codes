c837a8776c44f0c0aba7d1f54e362f0e877e12c6
Francisco <fgsj@cin.ufpe.br>
2021-03-19 23:48:23 -0300

    Checking what bigger number and smaller number

---
+++ src/Java_Condition/Code_Seven.java
@@ -0,0 +1,50 @@
+package Java_Condition;
+
+import java.util.Scanner;
+
+public class Code_Seven {
+    public static void main(String[] args) {
+
+        //Calling class "Scanner" for receiving data user input
+        Scanner input = new Scanner(System.in);
+
+
+        //Variables
+        int numberOne;
+        int numberTwo;
+        int numberThree;
+
+        //Receiving data user input
+        System.out.println("Enter number one: ");
+        numberOne = input.nextInt();
+
+        System.out.println("Enter number two: ");
+        numberTwo = input.nextInt();
+
+        System.out.println("Enter number three: ");
+        numberThree = input.nextInt();
+
+
+        if(numberOne > numberTwo && numberOne > numberThree && numberTwo > numberThree){
+            System.out.println("Number one is bigger " +numberOne);
+            System.out.println("Smaller number is three " + numberThree);
+        } else if (numberOne > numberTwo && numberOne > numberThree && numberThree > numberTwo){
+            System.out.println("Number one is bigger " + numberOne);
+            System.out.println("Smaller number is two " + numberTwo);
+        } else if (numberTwo > numberThree && numberTwo > numberOne && numberThree > numberOne){
+            System.out.println("Number Two is bigger " + numberTwo);
+            System.out.println("Smaller number is one " + numberOne);
+        } else if (numberTwo > numberThree && numberTwo > numberOne && numberOne > numberThree){
+            System.out.println("Number Two is bigger " + numberTwo);
+            System.out.println("Smaller number is three " + numberThree);
+        } else if (numberThree > numberOne && numberThree > numberTwo && numberOne > numberTwo){
+            System.out.println("Number Three is bigger " + numberThree);
+            System.out.println("Smaller number is two " + numberTwo);
+        } else if (numberThree > numberOne && numberThree > numberTwo && numberTwo > numberOne) {
+            System.out.println("Number Three is bigger " + numberThree);
+            System.out.println("Smaller number is two " + numberOne);
+        }
+
+
+    }
+}
