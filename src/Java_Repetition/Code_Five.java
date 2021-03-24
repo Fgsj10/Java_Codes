6b6be755af899abaceeb8041a4eb5220feef189b
Francisco <fgsj@cin.ufpe.br>
2021-03-23 22:47:14 -0300

    Printing numbers between 1 and 20

---
+++ src/Java_Repetition/Code_Five.java
@@ -0,0 +1,22 @@
+package Java_Repetition;
+
+import java.util.Scanner;
+
+/***
+ * @author Francisco Junior
+ */
+
+public class Code_Five {
+    public static void main(String[] args) {
+        //Calling class "Scanner" for received data user input
+        Scanner input = new Scanner(System.in);
+
+        //Creating variables
+        int number;
+
+        //Logical loop for printing numbers
+        for(number = 0; number <= 20; number++ ){
+            System.out.println(number);
+        }
+    }
+}
